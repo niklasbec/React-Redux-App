33f7b8e5a458447fe59ffc75a40974c729355849
niklasbec <niklasbeckerr@gmail.com>
2019-12-12 16:32:19 +0100

    done styling

--- app/src/App.js
+++ app/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 import { increment, getKanyeWisdom } from './state/actionCreators'
-
+import kanye1 from './images/kanye1.png'
+import kanye2 from './images/kanye2.png'
 
 const Button = styled.a`
 
   display: inline-block;
   border-radius: 3px;
-  padding: 0.5rem 0;
+  padding: 0.8rem 0;
   margin: 0.5rem 1rem;
   width: 11rem;
   background: black;
@@ -27,11 +28,15 @@ function App({increment, count, getKanyeWisdom, wisdom}) {
 
   return (
     <div className="App">
-      <h2>Kanye says:</h2>
+      <div className='header-block'>
+      <h2>Kanye Wisdom</h2>
+      <img className='kanye-img' src={kanye1} width='200px'/>
+      </div>
       <div className='kanye-quote'>
       <p className='wisdom'>{wisdom}</p>
       </div>
       <Button onClick={getKanyeWisdom}>Kanye, tell me more!</Button>
+      <img className='kanye-img2' src={kanye2} width='200px'/>
     </div>
   );
 }
